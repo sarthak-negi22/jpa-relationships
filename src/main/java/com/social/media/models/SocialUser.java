e75409a5a4483c4d8efef50141a73d701a143306
Sarthak <saryatnam22@gmail.com>
2025-03-17 23:13:53 +0530

    one to many and many to one mapping

--- src/main/java/com/social/media/models/SocialUser.java
+++ src/main/java/com/social/media/models/SocialUser.java
@@ -2,6 +2,9 @@ package com.social.media.models;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class SocialUser {
 
@@ -12,4 +15,7 @@ public class SocialUser {
     @OneToOne
     @JoinColumn(name = "social_profile_id")     // Only used in the "owning" entity, since SocialUser owns the relationship
     private SocialProfile socialProfile;
+
+    @OneToMany(mappedBy = "socialUser")
+    private List<SocialPost> socialPosts = new ArrayList<>();
 }
